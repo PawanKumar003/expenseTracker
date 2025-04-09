074749f3cc0c781b0ea09fed8a5fdb721051e401
Pawan Kumar <pk06181@gmail.com>
2025-04-09 22:53:00 +0530

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,181 @@
+import { useEffect, useId, useState } from "react";
+import Modal from "./components/Modal";
+
+function App() {
+  const [item, setItem] = useState("");
+  const [num, setNum] = useState("");
+  const [desc, setDesc] = useState("");
+  const [obj, setObj] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [editdata, setEditData] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setObj((prev) => [...prev, { id: Date.now(), item, num, desc }]);
+  };
+  const handleDelete = (event, id) => {
+    event.preventDefault();
+    let fltr = obj.filter((todo) => todo.id != id);
+    setObj(fltr);
+  };
+  const handleEdit = (item) => {
+    setEditData(item);
+    setShowModal(true);
+    // console.log(e, "dedit");
+  };
+  return (
+    <>
+      <Modal
+        showModal={showModal}
+        setShowModal={setShowModal}
+        editdata={editdata}
+      />
+      <div className="text-left">
+        <h1 className="text-3xl font-bold underline text-center">
+          Hello World
+          {item}
+          {num}
+          {desc}
+        </h1>
+        <form onSubmit={handleSubmit} className="max-w-md mx-auto">
+          <div className="relative z-0 w-full mb-5 group">
+            <input
+              onChange={(e) => setItem(e.target.value)}
+              type="text"
+              name="floating_first_name"
+              id="floating_first_name"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              required
+            />
+            <label
+              htmlFor="floating_first_name"
+              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Enter Item
+            </label>
+          </div>
+          <div className="relative z-0 w-full mb-5 group">
+            <input
+              onChange={(e) => setNum(e.target.value)}
+              type="tel"
+              name="floating_last_name"
+              id="floating_last_name"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              required
+            />
+            <label
+              htmlFor="floating_last_name"
+              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Enter Number
+            </label>
+          </div>
+          <div className="relative z-0 w-full mb-5 group">
+            <textarea
+              onChange={(e) => setDesc(e.target.value)}
+              type="text"
+              name="floating_last_name"
+              id="floating_last_name"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              required
+            ></textarea>
+            <label
+              htmlFor="floating_last_name"
+              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Description
+            </label>
+          </div>
+          <button
+            type="submit"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+      <br></br>
+      <br></br>
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg w-100 ">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="p-4">
+                <div className="flex items-center">
+                  <input
+                    id="checkbox-all-search"
+                    type="checkbox"
+                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                  />
+                  <label htmlFor="checkbox-all-search" className="sr-only">
+                    checkbox
+                  </label>
+                </div>
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Iten Name
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Number
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Description
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Action
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {obj.map((item, i) => (
+              <tr
+                key={i}
+                className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600"
+              >
+                <td className="w-4 p-4">
+                  <div className="flex items-center">
+                    <input
+                      id="checkbox-table-search-1"
+                      type="checkbox"
+                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                    />
+                    <label
+                      htmlFor="checkbox-table-search-1"
+                      className="sr-only"
+                    >
+                      checkbox
+                    </label>
+                  </div>
+                </td>
+                <td
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                >
+                  {item.item}
+                </td>
+
+                <td className="px-6 py-4">{item.num}</td>
+                <td className="px-6 py-4">{item.desc}</td>
+                <td className="flex items-center px-6 py-4">
+                  <button onClick={() => handleEdit(item)}>Edit</button>
+                  <a
+                    onClick={(event) => handleDelete(event, item.id)}
+                    href="#"
+                    className="font-medium text-red-600 dark:text-red-500 hover:underline ms-3"
+                  >
+                    Remove
+                  </a>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+}
+
+export default App;
